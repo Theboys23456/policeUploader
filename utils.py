c4c3d38729e897bb5740c5d0015e7fc7957f0435
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-08-19 12:01:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,8 +7,6 @@ from vars import CREDIT #NIKHIL SAINI BOTS
 from pyrogram.errors import FloodWait #NIKHIL SAINI BOTS
 from datetime import datetime,timedelta #NIKHIL SAINI BOTS
 
-import subprocess, os
-
 def cut_first_13_seconds(input_file):
     output_file = input_file  # overwrite karenge
     temp_file = input_file + ".tmp.mp4"
@@ -128,6 +126,7 @@ async def progress_bar(current, total, reply, start): #NIKHIL SAINI BOTS
                  #await reply.edit(f'`╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣ \n┣⪼ ⚡{progress_bar} : {perc}\n┣⪼ 🚀 sᴘᴇᴇᴅ : {sp} \n┣⪼ 📟 ᴘʀᴏᴄᴇssᴇᴅ : {cur}\n┣⪼ 💾 sɪᴢᴇ- ᴇᴛᴀ :  {tot} : {eta} \n╰━⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 ❝𝐂𝐡𝐚𝐦𝐛𝐚𝐥 𝐇𝐢𝐭𝐥𝐞𝐫𝐬 ❥❥═══ ❤️:)™~⌋─━━➣`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
+
 
 
 
