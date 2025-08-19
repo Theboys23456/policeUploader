afd74cf6779954c7db658b48ebe12372d50d8934
Abhisehkkiar15 <mpsystem05@gmail.com>
2025-08-19 11:02:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,13 +10,20 @@ from datetime import datetime,timedelta #NIKHIL SAINI BOTS
 def cut_first_13_seconds(input_file, output_file=None):
     import subprocess, os
     if output_file is None:
-        name, ext = os.path.splitext(input_file)
-        output_file = input_file
+        temp_file = input_file + ".tmp.mp4"
+    else:
+        temp_file = output_file
 
-    cmd = ["ffmpeg", "-ss", "13", "-i", input_file, "-c", "copy", "-y", output_file]
+    cmd = [
+        "ffmpeg", "-ss", "13", "-i", input_file,
+        "-c:v", "libx264", "-c:a", "aac", "-y", temp_file
+    ]
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    return output_file if os.path.exists(output_file) else input_file
+    if os.path.exists(temp_file):
+        os.replace(temp_file, input_file)  # पुरानी फाइल replace हो जाएगी
+        return input_file
+    return input_file
     
     
 
@@ -118,6 +125,7 @@ async def progress_bar(current, total, reply, start): #NIKHIL SAINI BOTS
                  #await reply.edit(f'`╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣ \n┣⪼ ⚡{progress_bar} : {perc}\n┣⪼ 🚀 sᴘᴇᴇᴅ : {sp} \n┣⪼ 📟 ᴘʀᴏᴄᴇssᴇᴅ : {cur}\n┣⪼ 💾 sɪᴢᴇ- ᴇᴛᴀ :  {tot} : {eta} \n╰━⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 ❝𝐂𝐡𝐚𝐦𝐛𝐚𝐥 𝐇𝐢𝐭𝐥𝐞𝐫𝐬 ❥❥═══ ❤️:)™~⌋─━━➣`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
+
 
 
 
